56ca361a6dcad93749388d0989da47d05635bfa1
Karolina Kuchta <linka_94@interia.eu>
2019-10-25 15:14:42 +0200

    Add lifecycle methods console logs

--- Counter.js
+++ Counter.js
@@ -1,8 +1,32 @@
 var Counter = React.createClass({
+    getDefaultProps: function() {
+        return console.log('Metoda getDefaultProps()');
+    },
     getInitialState: function() {
         return {
             counter: 0
         };
+    },
+    componentWillMount: function() {
+        console.log('Metoda componentWillMount()');
+    },
+    componentDidMount: function() {
+        console.log('Metoda componentDidMount()');
+    },
+    componentWillReceiveProps: function(nextProps) {
+        console.log('Metoda componentWillReceiveProps()');
+    },
+    shouldComponentUpdate: function(nextProps, nextState) {
+        console.log('Metoda shouldComponentUpdate()');        
+    },
+    componentWillUpdate: function(nextProps, nextState) {
+        console.log('Metoda componentWillUpdate()');
+    },
+    componentDidUpdate: function(prevProps, prevState) {
+        console.log('Metoda componentDidUpdate()');
+    },
+    componentWillUnmounth: function() {
+        console.log('componentWillUnmounth');
     },
     increment: function() {
         this.setState({
@@ -15,13 +39,14 @@ var Counter = React.createClass({
         });
     },
 
-    render: function() {
+    render: function() {        
         return React.createElement('div', {},
             React.createElement('span', {}, 'Licznik: ' + this.state.counter),
             React.createElement('div', {}, 
                 React.createElement('button', {onClick: this.decrement}, 'Odejmij'),
                 React.createElement('button', {onClick: this.increment}, 'Dodaj')
-            )
+            ),
+            console.log('Renderowanie widoku komponentu')
         );
     }
 });
